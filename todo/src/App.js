e38ebd389b5eec707b7203ab2a90e7a6cab28149
Naomi <naomipriest@rocketmail.com>
2020-03-16 18:11:04 -0400

    saving my working todo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Task from './components/Task';
+//import TaskForm from './components/TaskForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Task />
     </div>
   );
 }
---
+++ todo/src/components/Task.js
@@ -0,0 +1,59 @@
+import React, { useState, useReducer } from "react";
+import { taskReducer, initialState } from "../reducers/taskReducer";
+
+const Task = () => {
+ 
+  const [state, dispatch] = useReducer(taskReducer, initialState);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChanges = e => {
+    setNewTask(e.target.value);
+  };
+
+  return (
+    <div>
+        <h2>TODO LIST USING REDUCERS</h2>
+       <label>New To-Do</label>
+       <input 
+       type="text"
+       name="newTaskName"
+       value={newTask}
+       onChange={handleChanges}
+       />
+       <button
+       onClick={() => dispatch({
+           type: "ADD_ITEM",
+           payload: {item: newTask}
+       })}>
+           Add Item
+       </button>
+       <button
+       onClick={() => dispatch({
+           type: "CLEAR_DONE"
+       })}>
+           Clear Completed Todos
+       </button>
+
+       <div className="todo-list">
+           <ul>
+               {state.map(item => (
+                   <li>
+                       <div className={`todo-item${item.completed? "done" : ""}`}
+                       onClick={() => dispatch({
+                           type:"TOGGLE_COMPLETED",
+                           payload: item.id
+                       })}>
+                           {item.item}
+                       </div>
+                       
+                   </li>
+               ))}
+           </ul>
+       </div>
+       
+     
+    </div>
+  );
+};
+
+export default Task;
